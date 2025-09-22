e3b348e5d2cd75fdd8471d189c161a8f4c219cd7
ares <stefare@hotmail.it>
2025-09-23 01:25:27 +0200

    First implementation of lyric translation Updated getLyrics to generate and return lyric translation if translation is required and if lyrics has a different lenguage.

--- lib/services/lyrics.dart
+++ lib/services/lyrics.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart';
+import 'package:translator/translator.dart';
+import 'package:language_detector/language_detector.dart';
 
 class Lyrics {
   Map lyricsList = {};
@@ -10,21 +12,43 @@ class Lyrics {
     required int durationInSeconds,
     String? artist,
     String? album,
+    String? translation,
   }) async {
     if (lyricsList[videoId] != null) {
       return lyricsList[videoId];
     }
     Map response = await fetchLyrics(
-        videoId: videoId,
-        title: title,
-        durationInSeconds: durationInSeconds,
-        album: album,
-        artist: artist);
+      videoId: videoId,
+      title: title,
+      durationInSeconds: durationInSeconds,
+      album: album,
+      artist: artist,
+    );
     if (response["syncedLyrics"] != null || response['plainLyrics'] != null) {
       final lyricsData = {
-        "syncedLyrics": response["syncedLyrics"],
-        "plainLyrics": response["plainLyrics"]
+        "syncedLyrics": response["syncedLyrics"].toString(),
+        "plainLyrics": response["plainLyrics"],
+        "transLyrics": "",
       };
+      if (translation != null) {
+        String lyricsLang = await LanguageDetector.getLanguageCode(
+            content: response["plainLyrics"]);
+        if (lyricsLang != translation) {
+          if (response["syncedLyrics"] != null) {
+            lyricsData["transLyrics"] = await translateSyncLyrics(
+              response["syncedLyrics"].toString(),
+              lyricsLang,
+              translation,
+            );
+          } else {
+            lyricsData["plainLyrics"] = await translatePlainLyrics(
+              response['plainLyrics'],
+              lyricsLang,
+              translation,
+            );
+          }
+        }
+      }
       lyricsList[videoId] = lyricsData;
       return lyricsData;
     }
@@ -68,5 +92,44 @@ class Lyrics {
       lyric = decoded.isNotEmpty ? decoded.first : {};
     }
     return lyric;
+  }
+}
+
+Future<String?> translateSyncLyrics(
+    String lyric, String from, String to) async {
+  try {
+    Translation trans = await lyric.translate(from: from, to: to);
+    final transLines = trans.text.split("\n");
+    final lyricLines = lyric.split("\n");
+    if (lyricLines.length != transLines.length) {
+      throw Exception("Translation lines do not match original lines");
+    }
+    String transLyric = "";
+    for (int i = 0; i < transLines.length; i++) {
+      transLyric +=
+          "${lyricLines[i].split("]")[0]}]${transLines[i].split("]")[1]}\n";
+    }
+    return transLyric;
+  } catch (e) {
+    return "";
+  }
+}
+
+Future<String?> translatePlainLyrics(
+    String lyric, String from, String to) async {
+  try {
+    Translation trans = await lyric.translate(from: from, to: to);
+    final lines = lyric.split("\n");
+    final transLines = trans.text.split("\n");
+    if (lines.length != transLines.length) {
+      throw Exception("Translation lines do not match original lines");
+    }
+    String transLyric = '';
+    for (int i = 0; i < lines.length; i++) {
+      transLyric += "${lines[i]}\n[${transLines[i]}]\n\n";
+    }
+    return transLyric;
+  } catch (e) {
+    return "";
   }
 }
